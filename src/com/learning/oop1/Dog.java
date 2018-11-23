a947d3d7138d65fc87fc1ca84d136c91edc02017
AndreiVRP <andrei.hrabovich.vrp@gmail.com>
2018-11-23 16:13:23 +0300

    Lessons 17-18. Static, Modifiers.

--- src/com/learning/oop1/Dog.java
+++ src/com/learning/oop1/Dog.java
@@ -1,14 +1,20 @@
 package com.learning.oop1;
 
 public class Dog {
+    private static int dogsCount;
+
     private int paws = 4;
     private int tail = 1;
     private String name;
     private String breed;
+    private String size;
 
-    public Dog(String name, String breed) {
+    public Dog(String name, String breed, String size) {
         this.name = name;
         this.breed = breed;
+        this.size = size;
+        //increases the amount of dogs each time a new instance is created
+        dogsCount++;
     }
 
     public String getName() {
@@ -36,8 +42,54 @@ public class Dog {
         }
     }
 
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        if (size.equalsIgnoreCase("Big") ||
+                size.equalsIgnoreCase("Average") ||
+                size.equalsIgnoreCase("Small")) {
+            this.size = size;
+        } else {
+            System.out.println("The size should be one of these: Big. Average, or  Small");
+        }
+    }
+
     public void getInfo() {
         System.out.println("This is " + this.name + ", and its breed is " + this.breed);
     }
+
+    public void bark() {
+        switch (size) {
+            case "Big":
+                System.out.println("Woof! Woof!");
+                break;
+            case "Average":
+                System.out.println("Raf! Raf!");
+                break;
+            case "Small":
+                System.out.println("Yip! Yip!");
+                break;
+            default:
+                System.out.println(".....");
+                break;
+        }
+
+
+    }
+
+    public static void getDogsCount() {
+        if (dogsCount <= 0) {
+            System.out.println("Currently, there are no dogs out there!");
+        } else if (dogsCount == 1) {
+            System.out.println("Currently, there is only one dog out there!");
+        } else {
+            System.out.println("Currently, there are " + dogsCount + " dogs out there!");
+        }
+        System.out.println();
+
+    }
+
 
 }
--- src/com/learning/oop1/Main.java
+++ src/com/learning/oop1/Main.java
@@ -1,19 +1,33 @@
 package com.learning.oop1;
+import com.learning.oop1.Dog;
 
 public class Main {
     public static void main(String[] args) {
-        Dog lab = new Dog("Rex", "Lab");
+
+        Dog.getDogsCount();
+
+        Dog lab = new Dog("Rex", "Lab", "Average ");
         lab.getInfo();
         lab.setName("Charlie");
         lab.getInfo();
         System.out.println(lab.getName());
+        lab.bark();
 
-        Dog shepherd = new Dog("Mike", "Shepherd");
+        Dog.getDogsCount();
+
+        Dog shepherd = new Dog("Mike", "Shepherd", "Almost Big");
         shepherd.getInfo();
         shepherd.setTail(2);
         shepherd.setTail(0);
         shepherd.setTail(-1);
         shepherd.setTail(1);
         System.out.println("The dog named " + shepherd.getName() + " has " + shepherd.getTail() + " tail");
+        shepherd.bark();
+        shepherd.setSize("Big");
+        shepherd.bark();
+
+        Dog.getDogsCount();
+
+
     }
 }
