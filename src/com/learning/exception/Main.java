3d8636ce6f0812fe03949ae6d84542d08f9bd394
AndreiVRP <andrei.hrabovich.vrp@gmail.com>
2018-11-27 17:14:36 +0300

    Lesson 34-1. Checked and Unchecked Exceptions.

--- src/com/learning/exception/Main.java
+++ src/com/learning/exception/Main.java
@@ -1,5 +1,8 @@
 package com.learning.exception;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -15,16 +18,16 @@ public class Main {
                 System.out.println("Please, enter a denominator");
                 int denominator = sc.nextInt();
                 System.out.println("The result of dividing " + numerator + " by " + denominator + " is " + divide(numerator, denominator));
+                saveToFile(divide(numerator,denominator));
                 continueLoop = false;
-            } catch (ArithmeticException e) {
+            } catch (ArithmeticException | InputMismatchException e) {
                 System.out.println("Exception: " + e);
                 sc.nextLine();
-                System.out.println("Non-zero params only");
+                System.out.println("Integer non-zero params only");
                 // do not need to set continueLoop to true in catch blocks since it's initially true upon init
-            } catch (InputMismatchException e) {
-                System.out.println("Exception: " + e);
-                sc.nextLine();
-                System.out.println("Integer params only");
+            } catch (IOException e) {
+                System.out.println("Unable to open the file");
+                e.printStackTrace();
             }
         } while (continueLoop);
         System.out.println("The try-catch block is finished"); // termination model of exception handling
@@ -32,5 +35,11 @@ public class Main {
 
     private static double divide(int numerator, int denominator) {
         return numerator / denominator;
+    }
+
+    private static void saveToFile(double res) throws IOException {
+        PrintWriter writer = new PrintWriter(new FileWriter("out.txt"));
+        writer.println("The result is " + res);
+        writer.close();
     }
 }
