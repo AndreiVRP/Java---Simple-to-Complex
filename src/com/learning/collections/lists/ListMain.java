f6d6667d9619559551e7a22b64bfedb8978343cf
AndreiVRP <andrei.hrabovich.vrp@gmail.com>
2018-11-28 18:10:09 +0300

    Lessons 41-43. ArrayLists. ToDo App - MVP.
    This is a minimum viable product version of the console todo app, needs brushing up

---
+++ src/com/learning/collections/lists/ListMain.java
@@ -0,0 +1,28 @@
+package com.learning.collections.lists;
+
+public class ListMain {
+    public static void main(String[] args) {
+        ToDoList list1 = new ToDoList();
+        list1.addTask("test task 1");
+        list1.addTask("test task 2");
+        list1.addTask("test task 3");
+        list1.printToDoList();
+
+        ToDoList list2 = new ToDoList();
+        list2.addTask("to learn the Collections framework");
+        list2.addTask("to learn Generics");
+        list2.addTask("to revise everything");
+        list2.addTask("to move on to Selenium");
+        list2.printToDoList();
+        list2.addTask("test task");
+        list2.printToDoList();
+        list2.changeTask(5, "test task changed");
+        list2.printToDoList();
+        list2.addTask("to complete the Selenium course by Dec 25");
+        list2.printToDoList();
+        list2.removeTask(5);
+        list2.printToDoList();
+
+
+    }
+}
---
+++ src/com/learning/collections/lists/ToDoList.java
@@ -0,0 +1,34 @@
+package com.learning.collections.lists;
+
+import java.util.*;
+
+public class ToDoList {
+
+    private List<String> toDoList = new ArrayList<>();
+
+    public void addTask(String task) {
+        toDoList.add(task);
+        System.out.println("Task added \n");
+    }
+
+    public void changeTask(int taskNumber, String task) {
+        toDoList.set(taskNumber - 1, task);
+        System.out.println("Task number " + (taskNumber) + " has been changed to:");
+        System.out.println(task + "\n");
+    }
+
+    public void removeTask(int taskNumber) {
+        toDoList.remove(taskNumber - 1);
+        System.out.println("Task number " + (taskNumber) + " has been removed");
+//        System.out.println("Task 6 has been moved to line 5 \n");
+    }
+
+    public void printToDoList() {
+        System.out.println("----- TO DO -----");
+        for (String item : toDoList) {
+            System.out.println("Task " + (toDoList.indexOf(item) + 1) + " - " + item);
+        }
+        System.out.println("------------------");
+        System.out.println(toDoList.size() + " items to do \n");
+    }
+}
